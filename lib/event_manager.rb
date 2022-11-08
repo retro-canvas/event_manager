c43e6981dc52c010e219d58099d8aa99fcc9781f
retro-canvas <mahdigh@live.com>
2022-11-08 11:18:03 +0330

    initialized templating using ERB

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,29 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
+
+template_letter = File.read('form_letter.html')
+
 
 puts 'Event Manager is initialized!'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 # File.exists?('event_attendees.csv') ? (contents = File.read('event_attendees.csv')) : (puts "Error: No associated files detected!")
@@ -19,25 +35,25 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
+  legislators = legislators_by_zipcode(zipcode)
 
-    legislator_names = legislators.map do |legislator|
-      legislator.name
-    end
-    legislator_string = legislator_names.join(", ")
-  rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  form_letter = erb_template.result(binding)
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 
-  puts "#{name} #{zipcode} #{legislator_string}"
 end
