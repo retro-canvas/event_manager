bf11709d18f997575bb8722734db13b9c5c1c583
retro-canvas <mahdigh@live.com>
2022-11-08 10:38:11 +0330

    imbedded google civil API alongside cleaner code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,43 @@
-puts "Event Manager Initialized!"
+require 'csv'
+require 'google/apis/civicinfo_v2'
 
-File.exists?('event_attendees.csv') ? (contents = File.read('event_attendees.csv')) : (puts "Error: No associated files detected!")
+puts 'Event Manager is initialized!'
 
-puts contents
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+# File.exists?('event_attendees.csv') ? (contents = File.read('event_attendees.csv')) : (puts "Error: No associated files detected!")
+# puts contents
+
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map do |legislator|
+      legislator.name
+    end
+    legislator_string = legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislator_string}"
+end
